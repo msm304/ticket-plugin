c2e43f37c8a92484d34f20b2b5bca7883c7b8fce
msm304 <soltani5747@gmail.com>
2023-05-13 20:28:48 +0330

    Create notice in admin panel for check php version

--- core.php
+++ core.php
@@ -7,25 +7,32 @@ Author: Amirhosein Soltani
 Version: 1.0.0
 Author URI: https://owebra.com
 */
+
 defined('ABSPATH') || exit('Not Access');
 require 'inc/tkt-assets.php';
 class Core
 {
     private static $_instance = null;
-    public static function instance(){
-        if(is_null(self::$_instance)){
+    const MINIMUM_PHP_VERSION = '7.2';
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
             self::$_instance = new self();
         }
         return self::$_instance;
     }
     public function __construct()
     {
+        if (version_compare(7.1, self::MINIMUM_PHP_VERSION, '<')) {
+            add_action('admin_notices', [$this, 'admin_php_notice']);
+            return;
+        }
         $this->constants();
         $this->init();
     }
     public function constants()
     {
-        if(!function_exists('get_plugin_data')){
+        if (!function_exists('get_plugin_data')) {
             require_once(ABSPATH . 'wp-admin/includes/plugin.php');
         }
         define('TKT_BASE_FILE', __FILE__);
@@ -35,11 +42,12 @@ class Core
         define('TKT_FRONT_ASSETS', trailingslashit(plugin_dir_url(TKT_URL . 'assets/front')));
 
         $tkt_plugin_data = get_plugin_data(TKT_BASE_FILE);
-        define('TKT_VER' , $$tkt_plugin_data['Version']);
+        define('TKT_VER', $$tkt_plugin_data['Version']);
     }
-    public function init(){
-        register_activation_hook(TKT_BASE_FILE , [$this , 'active']);
-        register_deactivation_hook(TKT_BASE_FILE , [$this , 'deactive']);
+    public function init()
+    {
+        register_activation_hook(TKT_BASE_FILE, [$this, 'active']);
+        register_deactivation_hook(TKT_BASE_FILE, [$this, 'deactive']);
         new TKT_Assets();
     }
     public function active()
@@ -47,6 +55,14 @@ class Core
     }
     public function deactive()
     {
+    }
+    public function admin_php_notice()
+    { ?>
+        <div class="notice notice-warning">
+            <p> افزونه تیکت پشتیبانی برای اجرای صحیح نیاز به نسخه php 7.2 به بالا دارد، لطفا نسخه php هاست خود را ارتقا دهید.
+            </p>
+        </div>
+<?php
     }
 }
 
